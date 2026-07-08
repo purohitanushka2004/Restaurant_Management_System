1755264fb9c07236dbfe97f0347aa9657fc1d0f9
Priyanka Belwal <priyankabelwal09@gmail.com>
2026-07-08 12:09:30 +0530

    Add mail configuration debugging

--- server/src/main/java/com/edutech/RestrurantManagementSystemApplication.java
+++ server/src/main/java/com/edutech/RestrurantManagementSystemApplication.java
@@ -1,13 +1,13 @@
-
-
-
-
 package com.edutech;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.edutech.model.Role;
@@ -16,6 +16,18 @@ import com.edutech.repository.UserRepository;
 
 @SpringBootApplication
 public class RestrurantManagementSystemApplication {
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void checkMailConfig() {
+        System.out.println("=================================");
+        System.out.println("MAIL HOST = " + env.getProperty("spring.mail.host"));
+        System.out.println("MAIL PORT = " + env.getProperty("spring.mail.port"));
+        System.out.println("MAIL USER = " + env.getProperty("spring.mail.username"));
+        System.out.println("=================================");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestrurantManagementSystemApplication.class, args);
@@ -33,8 +45,9 @@ public class RestrurantManagementSystemApplication {
                 admin.setPassword(encoder.encode("admin123"));
                 admin.setRole(Role.ADMIN);
                 admin.setSessionId("ADMIN_SESSION");
+                admin.setEmail("purohitanushka2004@gmail.com");
+
                 userRepository.save(admin);
-				admin.setEmail("purohitanushka2004@gmail.com");
             }
         };
     }
